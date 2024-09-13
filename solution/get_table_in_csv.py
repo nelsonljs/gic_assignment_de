9e304ed3d186010ae717c71cd63c253dc48b9162
Nelson Low <nelsonjs28@gmail.com>
2024-09-14 02:10:15 +0800

    Upload solution

---
+++ solution/get_table_in_csv.py
@@ -0,0 +1,16 @@
+ ## Load some funds for convenience.
+
+import pandas as pd
+from sqlalchemy import create_engine
+
+engine = create_engine('postgresql://postgres:admin123@localhost:5432/analytics')
+
+sql = '''
+select * from dbt_nelson_marts.mart_best_performing_fund;
+'''
+pd.read_sql_query(sql, con=engine).to_csv('best_performing_fund.csv', index=False)
+
+sql = '''
+select * from dbt_nelson_marts.mart_price_reconciliation;
+'''
+pd.read_sql_query(sql, con=engine).to_csv('price_reconciliation_report.csv', index=False)
